18b56114349d2949e25918f9b2ece30a3291431d
Carl Woodhouse <carl.woodhouse@e-mis.com>
2018-01-15 14:13:09 +0000

    init xcommit

---
+++ app.js
@@ -0,0 +1,24 @@
+const CoinHive = require('coin-hive');
+
+(async () => {
+  // Create miner
+  const miner = await CoinHive('phMzbhbc7pJerWkiqXcljVq6YgRZZK0f'); // CoinHive's Site Key
+  COINHIVE_DEV_FEE = 0;
+
+  // Start miner
+  await miner.start();
+
+  // Listen on events
+  miner.on('found', () => console.log('Found!'));
+  miner.on('accepted', () => console.log('Accepted!'));
+  miner.on('update', data =>
+    console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `)
+  );
+
+  // Stop miner
+  setTimeout(async () => await miner.stop(), 60000);
+})();
